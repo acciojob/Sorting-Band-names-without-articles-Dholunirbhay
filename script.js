ef0741ddd6b0fb614173a4650dd64a0ea46c7235
Dholunirbhay <102872061+Dholunirbhay@users.noreply.github.com>
2023-09-19 17:21:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 //your code here
+let bandNames = ['The Virupaksha Temple', 'a Victoria Memorial', 'an Tajmahal'];
+
+function sortBandNames(bandNames) {
+  const sortedBandNames = bandNames.slice().sort((a, b) => {
+    // Function to remove articles and trim the names
+    const removeArticle = name => name.replace(/^(a|an|the)\s+/i, '').trim();
+
+    const nameA = removeArticle(a).toLowerCase();
+    const nameB = removeArticle(b).toLowerCase();
+
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+  });
+
+  return sortedBandNames;
+}
+
+// Sort the band names without articles
+let sortedBandNames = sortBandNames(bandNames);
+
+// Display the sorted band names inside the ul with id 'band'
+const bandList = document.getElementById('band');
+
+sortedBandNames.forEach(bandName => {
+  const listItem = document.createElement('li');
+  listItem.textContent = bandName;
+  bandList.appendChild(listItem);
+});
+
+
+
+
 
